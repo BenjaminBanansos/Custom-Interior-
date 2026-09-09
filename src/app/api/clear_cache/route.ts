1ce73a114860fe41e2370822f5e1851ab1890909
BenjaminBanansos <110691801+BenjaminBanansos@users.noreply.github.com>
2026-09-09 12:22:56 +0530

    Add cache clearing API route

---
+++ src/app/api/clear_cache/route.ts
@@ -0,0 +1,8 @@
+
+import { revalidatePath } from 'next/cache';
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+  revalidatePath('/', 'layout');
+  return NextResponse.json({ revalidated: true, message: 'Next.js Data Cache completely cleared!' });
+}
